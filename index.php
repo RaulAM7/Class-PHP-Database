1d6c0f9d329a00cfd00d38e865889bcafd414ea1
RaulAM <raulartilesm@gmail.com>
2024-10-28 13:00:43 +0000

    Update

--- index.php
+++ index.php
@@ -9,17 +9,18 @@ $database = "Imperial_Database";
 $conn = mysqli_connect($servername, $username, $password, $database);
 
 // Check connection
-if (!$conn)
+if ($conn->connect_error)
 {
-    die("Connection Failed!". mysqli_connect_error());
+    die("Connection Failed!". $conn->connect_error);
 }
 echo "Connected successfully".PHP_EOL;
 
 
 // CRUD Operations (Create, Read (All, One), Update, Delete)
 
-// Create
+// CREATE
 // Write the query
+/*
 $sql0 = "
 INSERT INTO 
 Ships 
@@ -34,6 +35,24 @@ if (mysqli_query($conn, $sql0)){
 } else {
     echo 'ERROR'. mysqli_error($conn)-PHP_EOL;
 }
+*/
 
+// OTRA FORMA DE CREAR REGISTROS EN UNA DB
 
+$sql1 = "
+INSERT INTO 
+Planets 
+(planet_id, planet_name, region, population, discovered_date) 
+VALUES 
+(11, 'Planeta Tierra', 'Core Worlds', 7000000000, '1999-01-01')
+"
+;
 
+if ($conn->query($sql1))
+{
+    echo "New record created sucessfully".PHP_EOL;
+}
+else
+{
+    echo "New Error: ". $sql . $conn->error.PHP_EOL;
+}

bbacc9da33d428eb8a35216b76564d4359f677f3
RaulAM <raulartilesm@gmail.com>
2024-10-28 12:52:53 +0000

    Update

--- index.php
+++ index.php
@@ -13,6 +13,27 @@ if (!$conn)
 {
     die("Connection Failed!". mysqli_connect_error());
 }
-echo "Connected successfully";
+echo "Connected successfully".PHP_EOL;
+
+
+// CRUD Operations (Create, Read (All, One), Update, Delete)
+
+// Create
+// Write the query
+$sql0 = "
+INSERT INTO 
+Ships 
+(ship_id, ship_name, ship_type, capacity, in_service) 
+VALUES 
+(13, 'El Corsario Verde', 'Starfighter', 10, 'Independent')
+";
+
+// Executing the query
+if (mysqli_query($conn, $sql0)){
+    echo 'New record created successfully'.PHP_EOL;
+} else {
+    echo 'ERROR'. mysqli_error($conn)-PHP_EOL;
+}
+
 
 
